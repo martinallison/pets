b8733cf0724a9f5b84178df85b3adf595c5e55ba
Martin Allison <martin.allison@octoenergy.com>
2022-01-25 22:07:03 +0000

    Set up project

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<main>
+	<h1>Pets</h1>
+
+	<p>Track your petz.</p>
+</main>
+
+<style>
+	main {
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
